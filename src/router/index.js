2721d71980bdd7a645c6377566bb69eaa24331c7
Rius Sabarno <riussabarno12@gmail.com>
2020-01-10 12:54:13 +0700

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,68 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import Login from '../components/Login.vue'
+import Secure from '../components/Secure.vue'
+import Register from '../components/Register.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: Register
+  },
+  {
+    path: '/secure',
+    name: 'secure',
+    component: Secure,
+    meta: { 
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: About
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  if(to.matched.some(record => record.meta.requiresAuth)) {
+    if (store.getters.isLoggedIn) {
+      next()
+      return
+    }
+    next('/login') 
+  } else {
+    next() 
+  }
+})
+
+export default router
